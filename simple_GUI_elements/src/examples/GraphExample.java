9491637a1b61d1e28fe0e4791a37819dbc80a7ee
chris <christoph@roboterclub-freiburg.de>
2016-06-03 11:18:01 +0200

    GPL 3 copyright added

--- simple_GUI_elements/src/examples/GraphExample.java
+++ simple_GUI_elements/src/examples/GraphExample.java
@@ -35,3 +35,26 @@ public class GraphExample
 		
 	}
 }
+/* simple_GUI_elements
+ * © ChrisMicro 2016.
+ *
+ * This file is part of simple_GUI_elements.
+ *
+ * simple_GUI_elements is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * simple_GUI_elements is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with simple_GUI_elements.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you use simple_GUI_elements in public project you can inform me about this by e-mail,
+ * of course if you want it.
+ *
+ * web-site: https://github.com/ChrisMicro/java_simple_GUI_elements
+ */
